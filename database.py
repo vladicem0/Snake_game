90ebd3b5bdf0b9a564157040df1a278faa0b4e10
Vladislav <160229023+vladicem0@users.noreply.github.com>
2025-09-16 00:50:23 +0600

    Update database.py

--- database.py
+++ database.py
@@ -5,10 +5,10 @@ from functools import wraps
 def connection(func):
     """Decorator for connection/disconnection to/from database and save changes"""
     @wraps(func)
-    def wrapper(*args):
-        con = psycopg2.connect(database="postgres", user="postgres", password="TroLl554", host="127.0.0.1", port="5432")
+    def wrapper(*args, **kwargs):
+        con = psycopg2.connect(database="postgres", user="postgres", password="", host="127.0.0.1", port="5432")
         cur = con.cursor()
-        output = func(cur, *args)
+        output = func(cur, *args, **kwargs)
         con.commit()
         con.close()
         return output
@@ -65,5 +65,22 @@ def read_data(cur) -> dict[str, list[tuple[str, int]]]:
     return data
 
 
+@connection
+def _print_data(cur) -> None:
+    cur.execute("""SELECT DIFFICULTY, NICKNAME, SCORE FROM LEADERBOARD""")
+    data = {}
+    rows = cur.fetchall()
+    for row in rows:
+        try:
+            data[row[0]].append(row[1:])
+        except KeyError:
+            data[row[0]] = [row[1:]]
+    for k, v in data.items():
+        print('\n', k, sep='')
+        for value in v:
+            print(*value)
+
+
 if __name__ == '__main__':
+    #_print_data()
     print(read_data())
